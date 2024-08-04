4de029dcbe310691a39acdae246e8f948d3409c5
Matthew Hallonbacka <79469789+Mallonbacka@users.noreply.github.com>
2024-08-04 16:34:27 +0000

    Remove 'device' record

--- custom_components/digitransit/entity.py
+++ custom_components/digitransit/entity.py
@@ -1,4 +1,5 @@
 """DigitransitEntity class."""
+
 from __future__ import annotations
 
 from homeassistant.helpers.entity import DeviceInfo
@@ -6,6 +7,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import ATTRIBUTION, DOMAIN, NAME, VERSION
 from .coordinator import DigitransitDataUpdateCoordinator
+
 
 class DigitransitEntity(CoordinatorEntity):
     """A single configured instance of the component."""
@@ -16,9 +18,3 @@ class DigitransitEntity(CoordinatorEntity):
         """Initialize."""
         super().__init__(coordinator)
         self._attr_unique_id = coordinator.config_entry.entry_id
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, self.unique_id)},
-            name=NAME,
-            model=VERSION,
-            manufacturer=NAME,
-        )
