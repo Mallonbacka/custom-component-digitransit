3d2cd567493ec517a525c779adf76408caadb0dc
Matthew Hallonbacka <79469789+Mallonbacka@users.noreply.github.com>
2025-03-30 15:29:00 +0300

    Fix issue where scheduled and realtime departures are an hour early (#121)

--- custom_components/digitransit/utils.py
+++ custom_components/digitransit/utils.py
@@ -1,18 +1,26 @@
 """Transform data for display."""
-from datetime import datetime, timedelta
+
+from datetime import datetime
 import math
+
 
 def formatDepartureRow(row, timezone):
     """Simplify the departure information for use in the attribute."""
-    row['scheduledDeparture'] = datetime.fromtimestamp(row['serviceDay'], timezone) + timedelta(seconds=row['scheduledDeparture'])
-    row['realtimeDeparture'] = datetime.fromtimestamp(row['serviceDay'], timezone) + timedelta(seconds=row['realtimeDeparture'])
-    row['route'] = row.pop('trip')['routeShortName']
-    row.pop('serviceDay')
+    scheduledDepartureTimestamp = row["serviceDay"] + row["scheduledDeparture"]
+    row["scheduledDeparture"] = datetime.fromtimestamp(
+        scheduledDepartureTimestamp, timezone
+    )
+    realtimeDepartureTimestamp = row["serviceDay"] + row["realtimeDeparture"]
+    row["realtimeDeparture"] = datetime.fromtimestamp(
+        realtimeDepartureTimestamp, timezone
+    )
+    row["route"] = row.pop("trip")["routeShortName"]
+    row.pop("serviceDay")
     return row
+
 
 def departureToNumberOfMinutes(row):
     """Convert a departure into a number of minutes."""
-    departsAt = datetime.fromtimestamp(row['serviceDay'] + row['realtimeDeparture'])
+    departsAt = datetime.fromtimestamp(row["serviceDay"] + row["realtimeDeparture"])
     delta = departsAt - datetime.now()
     return math.floor(delta.total_seconds() / 60)
-
